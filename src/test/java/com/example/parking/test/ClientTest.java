e6915d596ce802b5146be0724bada42d2f03f808
Anton <ac983589@gmail.com>
2025-04-11 19:48:16 -0400

    Combined test files

--- src/test/java/com/example/parking/test/ClientTest.java
+++ src/test/java/com/example/parking/test/ClientTest.java
@@ -1,4 +1,4 @@
-package com.example.parking.tests;
+package com.example.parking.test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
